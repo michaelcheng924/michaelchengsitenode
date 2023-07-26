9b3d66fe6f65c9cd5bde9464338ea09f0c8d9e7b
Michael Cheng <cheng.c.mike@gmail.com>
2023-07-26 13:49:10 -0500

    add cats

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.MONGODB_URI), CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
